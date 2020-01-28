f07d9ce2cce4316cc934e3936af4fbfbb0ad2e1a
Bernd Vogt <bernd.vogt@sourcepit.org>
2020-01-28 18:15:26 +0100

    feat: add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate common_failures;
 extern crate caps;
 #[macro_use]
@@ -14,6 +16,8 @@ use common_failures::prelude::*;
 
 use caps::CapSet;
 use caps::Capability;
+use clap::App;
+use clap::Arg;
 use rcs::RemoteControl;
 use rcs::Switch;
 use rcs::SwitchCode::SwitchB;
@@ -27,6 +31,11 @@ use thread_priority::thread_native_id;
 use thread_priority::RealtimeThreadSchedulePolicy;
 use thread_priority::ThreadPriority;
 use thread_priority::ThreadSchedulePolicy;
+
+const ARG_REDIS_HOST: &str = "redis-host";
+const ARG_REDIS_PORT: &str = "redis-port";
+const ARG_NAME: &str = "name";
+const ARG_GPIO_PIN: &str = "gpio-pin";
 
 fn try_upgrade_thread_priority() -> Result<()> {
     let has_cap_sys_nice = match caps::has_cap(None, CapSet::Permitted, Capability::CAP_SYS_NICE) {
@@ -49,27 +58,67 @@ fn try_upgrade_thread_priority() -> Result<()> {
 }
 
 fn run() -> Result<()> {
+    let args = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name(ARG_REDIS_HOST)
+                .long(ARG_REDIS_HOST)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("localhost"),
+        )
+        .arg(
+            Arg::with_name(ARG_REDIS_PORT)
+                .long(ARG_REDIS_PORT)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("6379"),
+        )
+        .arg(
+            Arg::with_name(ARG_NAME)
+                .long(ARG_NAME)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("rcs"),
+        )
+        .arg(
+            Arg::with_name(ARG_GPIO_PIN)
+                .long(ARG_GPIO_PIN)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("17"),
+        )
+        .get_matches();
+
+    let redis_host = value_t!(args, ARG_REDIS_HOST, String)?;
+    let redis_port = value_t!(args, ARG_REDIS_HOST, usize)?;
+    let name = value_t!(args, ARG_NAME, String)?;
+    let gpio_pin = value_t!(args, ARG_GPIO_PIN, usize)?;
+
     // requires:
     // sudo setcap cap_sys_nice=ep <file>
     try_upgrade_thread_priority()?;
 
-    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let client = redis::Client::open(format!("redis://{}:{}", redis_host, redis_port))?;
     let mut con = client.get_connection()?;
 
-    let stream_name = "rcs";
-    
+    let stream_name = name;
     let result = redis::cmd("XRANGE")
-        .arg("rcs")
+        .arg(stream_name)
         .arg("-")
         .arg("+")
         .query(&mut con)?;
 
-    let pin = 17;
     let system_code = [true, true, true, false, false];
 
     let switch_b = Switch::new(&system_code, SwitchB);
 
-    let mut rc = RemoteControl::open(pin)?;
+    let mut rc = RemoteControl::open(gpio_pin)?;
 
     rc.send(&switch_b, On);
     thread::sleep(Duration::from_secs(2));
